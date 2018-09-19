8947b73f9705df40a028a509357a7805e03e378f
minghli <minghli@cisco.com>
2018-09-19 17:33:41 +0800

    assemble json

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -3,6 +3,7 @@ import { PlaylistService } from '../service/playlist.service';
 
 // 引入Lodash
 import * as _ from 'lodash';
+import { getLocaleTimeFormat } from '@angular/common';
 
 @Component({
   selector: 'app-playlist',
@@ -14,6 +15,7 @@ export class PlaylistComponent implements OnInit {
   listId;
   listTitle;
   listCover;
+  time;
 
   constructor(private playlistService: PlaylistService) { }
 
@@ -23,15 +25,25 @@ export class PlaylistComponent implements OnInit {
 
   getDetails(): void {
     this.playlistService.getDetails().subscribe(root => {
+
       console.log('获取到歌单json');
+
       // 歌单号
       this.listId = root.result.id;
+
       // 歌单名
       this.listTitle = root.result.name;
+
       // 歌单封面图
       this.listCover = root.result.coverImgUrl;
+
       // 拼装json
       const tracks = _.map(root.result.tracks, (track) => {
+
+        // 歌曲标题
+        const songTitle = track.name;
+
+        // 艺术家们
         const artists = track.artists;
         let songArtists = '';
         _.each(artists, (artist, index) => {
@@ -40,9 +52,26 @@ export class PlaylistComponent implements OnInit {
         // 去掉string尾指定字符
         songArtists = _.trimEnd(songArtists, ', ');
 
+        // 专辑名称
+        const album = track.album.name;
+
+        // 歌曲时长
+        let duration = track.duration;
+        duration = _.floor(duration / 1000);
+        let min: any = _.floor(duration / 60);
+        // 补全时间为2位数显示
+        if (min < 10) {
+          min = '0' + min;
+        }
+        const sec = duration % 60;
+        duration = min + ':' + sec;
+
+
         return {
-          'songTitle': track.name,
-          'songArtists': songArtists
+          'songTitle': songTitle,
+          'songArtists': songArtists,
+          'album': album,
+          'duration': duration,
         };
       });
 
