da5c873c1fbd1b07dc5eddbc00169319d0265400
minghli <minghli@cisco.com>
2018-09-19 14:43:01 +0800

    add lodash

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PlaylistService } from '../service/playlist.service';
 
+// 引入Lodash
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
