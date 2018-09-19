0b3000095380dd60d008a1ea8f34d981bd7357b4
minghli <minghli@cisco.com>
2018-09-19 16:48:35 +0800

    use lodash to assemble json

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -11,12 +11,11 @@ import * as _ from 'lodash';
 })
 export class PlaylistComponent implements OnInit {
 
-  constructor(private playlistService: PlaylistService) { }
-
   listId;
   listTitle;
-  tracks = [];
-  artists = [];
+  listCover;
+
+  constructor(private playlistService: PlaylistService) { }
 
   ngOnInit() {
     this.getDetails();
@@ -25,11 +24,29 @@ export class PlaylistComponent implements OnInit {
   getDetails(): void {
     this.playlistService.getDetails().subscribe(root => {
       console.log('获取到歌单json');
+      // 歌单号
       this.listId = root.result.id;
+      // 歌单名
       this.listTitle = root.result.name;
-      // this.tracks = root.result;
-      console.log(this.listId, ' ', this.listTitle);
-      console.log(root.tracks);
+      // 歌单封面图
+      this.listCover = root.result.coverImgUrl;
+      // 拼装json
+      const tracks = _.map(root.result.tracks, (track) => {
+        const artists = track.artists;
+        let songArtists = '';
+        _.each(artists, (artist, index) => {
+          songArtists = artist.name + ', ';
+        });
+        // 去掉string尾指定字符
+        songArtists = _.trimEnd(songArtists, ', ');
+
+        return {
+          'songTitle': track.name,
+          'songArtists': songArtists
+        };
+      });
+
+      console.log('tracks: ', tracks);
     });
   }
 
