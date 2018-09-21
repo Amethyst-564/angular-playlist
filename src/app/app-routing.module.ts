16fdc32804482ae5cec939317ca4fb7ec854e929
minghli <minghli@cisco.com>
2018-09-21 09:21:22 +0800

    edit default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,12 @@ import { SearchComponent } from './search/search.component';
 const routes: Routes = [
 
   // 默认路由
-  { path: '', redirectTo: '/playlist', pathMatch: 'full' },
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+
+  // 歌单组件
+  // { path: 'playlist', component: PlaylistComponent },
   { path: 'playlist/:id', component: PlaylistComponent },
-
-
-  // 歌单组件路由
-  { path: 'playlist', component: PlaylistComponent },
+  // 搜索组件
   { path: 'search', component: SearchComponent },
 ];
 
