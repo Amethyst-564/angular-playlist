0c43dc993557cdbf16394166083204bff5fe8434
minghli <minghli@cisco.com>
2018-09-22 15:09:15 +0800

    add login and global styles

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { Router } from '@angular/router';
+// 响应式表单
+import { FormGroup, FormControl } from '@angular/forms';
+// 表单验证器
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  model = new User('', '');
+
+
+  username = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log('调用登陆api成功');
   }
 
 }
