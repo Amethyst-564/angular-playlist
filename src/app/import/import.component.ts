f691032380dd78f96b9eb12b34efcf8994cdbdfd
minghli <minghli@cisco.com>
2019-01-03 18:39:27 +0800

    edit import

--- src/app/import/import.component.ts
+++ src/app/import/import.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as _ from 'lodash';
+declare var $;
+
 @Component({
   selector: 'app-import',
   templateUrl: './import.component.html',
@@ -7,7 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImportComponent implements OnInit {
 
-  placeHolderText = 'Choose your playlist file';
+  placeHolderText = '选择你的外部歌单文件';
+
+  msgType: string;
+  msgText: string;
 
   constructor() { }
 
@@ -22,7 +28,21 @@ export class ImportComponent implements OnInit {
   }
 
   changeFile(files: FileList) {
-    console.log(files);
+    const fileName = files[0].name;
+    const ext = _.last(fileName.split('.'));
+    if (ext === 'xlsx') {
+      console.log(fileName);
+    } else {
+      this.msgType = 'error';
+      this.msgText = '暂不支持此格式';
+      $('.toast').toast('show');
+    }
+  }
+
+  upload() {
+    this.msgType = 'success';
+    this.msgText = '从外部文件保存歌单成功';
+    $('.toast').toast('show');
   }
 
 }
